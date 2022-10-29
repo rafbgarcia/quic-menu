ff2671ce4e8b4c6b1a271987845323958f607cdd
Rafael Garcia <rafael@enzyme.com>
2022-10-29 15:44:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quic_menu',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
